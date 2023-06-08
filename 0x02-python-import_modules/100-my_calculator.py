8f2efc3cff07d6602b83e9ce32e0edc425f04a61
asonguilian <asonguillian@gmail.com>
2023-06-08 18:06:11 +0000

    Adds file for task 6 again

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     nargs = len(sys.argv) - 1
     if nargs != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.pu <a> <operator> <b>")
         sys.exit(1)
 
     op = sys.argv[2]
@@ -16,8 +15,8 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if op == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
+    if op =='+':
+        print("{} + {} = {}".format[a, b, add(a, b)))
     elif op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif op == '*':
